f3a4e9bdce73b01ac423e00acd4f614a90e70026
Etrama22 <muhazis73@gmail.com>
2024-07-08 13:39:52 +0700

    Search Bar

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -69,15 +69,40 @@ class _SearchBarState extends State<MovieSearchBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return Container(
+      padding: const EdgeInsets.fromLTRB(
+          25, 5, 25, 5), // add padding to avoid tight spacing
+      margin: const EdgeInsets.all(
+          16.0), // add margin to create space around the search bar
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 255, 255, 255),
+        borderRadius: BorderRadius.horizontal(
+          // create semi-circular corner
+          left: Radius.circular(50.0),
+          right: Radius.circular(50.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
       child: TextField(
         controller: _searchController,
         onChanged: _fetchMovies,
         decoration: InputDecoration(
+          border: InputBorder.none,
           hintText: 'Search for movies...',
+          hintStyle: TextStyle(color: Colors.grey),
           suffixIcon: _isLoading
-              ? CircularProgressIndicator()
+              ? Container(
+                  width: 24,
+                  height: 24,
+                  child: CircularProgressIndicator(),
+                )
               : _searchController.text.isNotEmpty
                   ? IconButton(
                       icon: Icon(Icons.clear),
